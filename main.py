78b2b31e2c8352da054391ad5caa17591a6eea45
Necro Mancer <necromancer@users.noreply.github.com>
2020-11-25 17:18:22 +0530

    base code tkinter

---
+++ main.py
@@ -0,0 +1,9 @@
+import os, sys
+from tkinter import *
+bg = '#ffffff'
+fg = '#000000'
+root = Tk()
+root.title('Radikl')
+root.attributes("-fullscreen", True)
+root.configure(bg=bg)
+root.mainloop()
