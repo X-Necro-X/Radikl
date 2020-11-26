32a65d1193a3891eb5861e07a37f9d771a21fa9a
Necro Mancer <necromancer@users.noreply.github.com>
2020-11-26 12:05:52 +0530

    cwd root access

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 # imports
-import os, sys, PIL.Image, PIL.ImageTk
+import os, PIL.Image, PIL.ImageTk
 from tkinter import *
 # functions
 def view(e, drive):
-    print(drive)
+    print(os.listdir(drive))
 def main():
     drives = ['%s:' % d for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists('%s:' % d)]
-    drive_icon = PIL.Image.open(os.getcwd()+'/drive.png')
-    drive_icon.thumbnail((200,200))
+    drive_icon = PIL.Image.open(basepath+'/drive.png')
+    drive_icon.thumbnail((size,size))
     img = PIL.ImageTk.PhotoImage(drive_icon)
     r, c = 0, 0
     for drive in drives:        
-        icon = Label(root, image=img)
+        icon = Label(root, image=img, bg=bg)
         icon.image = img
         icon.grid(row=r, column=c)
-        icon.bind("<Double-Button-1>", lambda e: view(e, drive))
-        label = Label(root, font=(None, 10), text=drive)
+        icon.bind("<Double-Button-1>", lambda e, d=drive: view(e, d))
+        label = Label(root, font=(None, 10), text=drive, bg=bg, fg=fg)
         label.grid(row=r+1, column=c)
         c += 1
         if not(c%7):
             c = 0
             r += 2
-    root.update()
 # tkinter globals
+size = 200
 bg = '#ffffff'
 fg = '#000000'
+title = 'Radikl'
+basepath = 'E:\Important\Radikl\Radikl'
 root = Tk()
-root.title('Radikl')
-root.attributes("-fullscreen", True)
-# root.configure(bg=bg)
+root.title(title)
+root.configure(bg=bg)
+for cd in range(len(os.getcwd().split('\\'))-1):
+    os.chdir('../')
 main()
 root.mainloop()
