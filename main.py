3048c524d568c68a61932073f8bef7138f20e6f0
Necro Mancer <necromancer@users.noreply.github.com>
2020-11-29 15:02:35 +0530

    scrollbar and lang name done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # imports
-import os, PIL.Image, PIL.ImageTk
+import os, PIL.Image, PIL.ImageTk, tkinter.ttk
 from tkinter import *
 # functions
 def iconLoader():
@@ -26,24 +26,33 @@ def view(e, location, stem):
     display(content)
 def display(content):
     stem = Frame(root)
+    stem.pack(fill=BOTH, expand=1)
+    mc = Canvas(stem)
+    mc.pack(side=LEFT, fill=BOTH, expand=1)
+    ms = tkinter.ttk.Scrollbar(stem, orient=VERTICAL, command=mc.yview)
+    ms.pack(side=RIGHT, fill=Y)
+    mc.configure(bg=bg, yscrollcommand=ms.set)
+    mc.bind('<Configure>', lambda e: mc.configure(scrollregion=mc.bbox('all')))
+    branch = Frame(mc)
+    mc.create_window((0, 0), window=branch, anchor='nw')
     stem.configure(bg=bg)
+    branch.configure(bg=bg)
     name, category = content
     r, c = 0, 0
     for item in range(len(content[0])):
         icon_choice = drive_icon if category[item]=='drive' else folder_icon if category[item]=='folder' else file_icon
-        icon = Label(stem, image=icon_choice, bg=bg)
+        icon = Label(branch, image=icon_choice, bg=bg)
         icon.image = icon_choice
         icon.grid(row=r, column=c)
         item_name = name[item].split('\\')
-        print(item_name)
         icon.bind("<Double-Button-1>", lambda e, d=name[item], s=stem: view(e, d, s))
-        label = Label(stem, font=(None, 10), text=item_name[len(item_name)-1] if item_name[len(item_name)-1] else item_name[0], bg=bg, fg=fg)
+        print_name = item_name[0] if not(item_name[len(item_name)-1]) else item_name[len(item_name)-1] if len(item_name[len(item_name)-1])<25 else item_name[len(item_name)-1][:24]+'...'
+        label = Label(branch, font=(None, 10), text=print_name, bg=bg, fg=fg)
         label.grid(row=r+1, column=c)
         c += 1
         if not(c%per_row):
             c = 0
             r += 2
-    stem.grid()
 def drive():
     drives = ['%s:\\' % d for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists('%s:' % d)]
     content = [drives, ['drive' for d in range(len(drives))]]
@@ -63,3 +72,4 @@ for cd in range(len(os.getcwd().split('\\'))-1):
     os.chdir('../')
 drive()
 root.mainloop()
+# rough
