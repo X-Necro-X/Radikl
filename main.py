44648ab7d16485e8d7f5f69089ff140b2074239e
Necro Mancer <necromancer@users.noreply.github.com>
2020-12-10 20:58:53 +0530

    button connect event set boilerplate

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ class Window(QtWidgets.QMainWindow):
         icon.setIcon(QtGui.QIcon('double-left.png'))
         icon.setIconSize(QtCore.QSize(self.layers[level][0].width()//5, self.layers[level][0].height()//5))
         icon.setFlat(True)
+        icon.clicked.connect(self.shiftLeft)
         layer = [3,3,2,2,1][len(items[0])-1] 
         for item in range(len(items[0])):
             icon = QtWidgets.QPushButton(self.layers[level][layer])
@@ -58,6 +59,7 @@ class Window(QtWidgets.QMainWindow):
             icon.setIcon(items[1][item])
             icon.setIconSize(items[1][item].availableSizes()[3])
             icon.setFlat(True)
+            icon.clicked.connect(lambda: self.openDir(1))
             name = QtWidgets.QLabel(self.layers[level][layer])
             name.setGeometry(0, 7*self.layers[level][layer].height()//10, self.layers[level][layer].width(), 2*self.layers[level][layer].height()//10)
             name.setText(items[0][item])
@@ -75,6 +77,13 @@ class Window(QtWidgets.QMainWindow):
         icon.setIcon(QtGui.QIcon('double-right.png'))
         icon.setIconSize(QtCore.QSize(self.layers[level][6].width()//5, self.layers[level][6].height()//5))
         icon.setFlat(True)
+        icon.clicked.connect(self.shiftRight)
+    def shiftLeft(self):
+        print('Left')
+    def shiftRight(self):
+        print('Right')
+    def openDir(self, item):
+        print(item)
     def extract(self, items):
         return [list(map(lambda x: self.model.fileName(self.model.index(x)), items)), list(map(lambda x: self.model.fileIcon(self.model.index(x)), items))]
 if __name__ == "__main__":
